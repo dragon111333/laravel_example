8495146e54b8feff4447911ceaa95c8392244f92
Thewin Thamma <65722117+dragon111333@users.noreply.github.com>
2021-08-14 10:01:54 +0700

    Update test_group.php

--- routes/test_group.php
+++ routes/test_group.php
@@ -1,1 +1,11 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
+Route::get("/",function(Request $res){
+    echo $res->name;
+    return response()->json([
+                'name' => 'this is group',
+                'state' => 'hahahah',
+            ]);
+});
